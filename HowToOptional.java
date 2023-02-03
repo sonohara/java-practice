1645d3914af7d049e90118d7b343ac9cde958b33
sonohara <is303038@gmail.com>
2023-02-03 17:54:49 +0900

    Optional

---
+++ HowToOptional.java
@@ -0,0 +1,105 @@
+import java.util.Optional;
+
+/**
+ * Optional
+ */
+public class HowToOptional {
+    public static void main(String[] args) {
+        // 生成
+        HowToOptional.of();
+        HowToOptional.ofNullable();
+        HowToOptional.empty();
+
+        // 取得
+        // 値がない場合は指定した値を返す
+        HowToOptional.orElse();
+        HowToOptional.orElseGet();
+        // 値がない場合に例外を投げる
+        HowToOptional.orElseThrow();
+        // 値が存在する場合のみ処理する、存在しない場合はなにもしない
+        HowToOptional.ifPresent();
+    }
+
+    protected static void of () {
+        System.out.println("of  ------------------");
+
+        // https://docs.oracle.com/javase/jp/17/docs/api/java.base/java/util/Optional.html#of(T)
+        // 引数が null だとぬるぽが発生する
+        Optional<String> o = Optional.of("hoge");
+        // Optional<String> o = Optional.of(null);
+        o.ifPresent(x -> System.out.println(x));
+    }
+
+    protected static void ofNullable() {
+        System.out.println("ofNullable  ------------------");
+
+        // https://docs.oracle.com/javase/jp/17/docs/api/java.base/java/util/Optional.html#ofNullable(T)
+        // こっちは引数が null でぬるぽは発生しない
+        Optional<String> o = Optional.ofNullable(null);
+        o.ifPresent(System.out::println);  // なにも出力されない
+    }
+
+    protected static void empty()  {
+        System.out.println("empty  ------------------");
+
+        // https://docs.oracle.com/javase/jp/17/docs/api/java.base/java/util/Optional.html#empty()
+        Optional<String> o = Optional.empty();
+        o.ifPresent(System.out::println);   // なにも出力されない
+    }
+
+    protected static void orElse() {
+        System.out.println("orElse  ------------------");
+
+        // https://docs.oracle.com/javase/jp/17/docs/api/java.base/java/util/Optional.html#orElse(T)
+        String p = "hoge";
+        // String p = null;
+        Optional<String> o = Optional.ofNullable(p);
+        String v = o.orElse(HowToOptional._getFuga());
+        // p = hoge の場合は hoge、p = null の場合は fuga が出力される
+        // orElse で指定した値は必ず評価されるため、p = hoge の場合でも _getFuga() は実行される（p = null の場合は当然実行される）
+        // -> 実行させたくない場合は orElseGet() を使う
+        System.out.println(v);
+    }
+
+    protected static void orElseGet() {
+        System.out.println("orElseGet  ------------------");
+
+        String p = "hoge";
+        // String p = null;
+        Optional<String> o = Optional.ofNullable(p);
+        // orElseと違って p = hoge の場合は _getFuga が call されない
+        String v = o.orElseGet(HowToOptional::_getFuga);
+
+        System.out.println(v);
+    }
+
+    protected static String _getFuga() {
+        System.out.println("call fuga");
+        return "fuga";
+    }
+
+    protected static void orElseThrow() {
+        System.out.println("orElseThrow  ------------------");
+
+        String p = "hoge";
+        // String p = null:
+        Optional<String> o = Optional.ofNullable(p);
+        try {
+            String v = o.orElseThrow(() -> new Exception("error"));
+            System.out.println(v);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+    
+    private static void ifPresent() {
+        System.out.println("ifPresent  ------------------");
+
+        // String p = "hoge";
+        String p = null;
+        Optional<String> o = Optional.ofNullable(p);
+        o.ifPresent(s -> System.out.println(s));
+        o.ifPresentOrElse(s -> System.out.println(s), () -> System.out.println("null"));
+    }
+}
--- Optional.md
+++ Optional.md
@@ -1,17 +1,30 @@
 ## Optional
-API
+
+### API
 
 https://docs.oracle.com/javase/jp/17/docs/api/java.base/java/util/Optional.html
 
 - 非null値を含んでも含まなくてもよいコンテナ・オブジェクト。 
 - `Optional` は、主に"検索結果はありません、"を明示的に表示する必要があり、`null` を使用するとエラーが発生する可能性のあるメソッドの戻り値の型として使用することを意図しています。 型が `Optional` の変数は、それ自体決して `null` であってはなりません。常に `Optional` インスタンスを指す必要があります。
 
+### 基本的な使い方
 
-Optionalクラスを意図されたとおりに使うための12のレシピ
+- [Java8 Optionalの逆引きレシピ](https://zenn.dev/nslender/articles/e2292eb9e06d35)
+- [今更まとめるJava Optional](https://zenn.dev/ayumukob/articles/552d0c7b544838)
 
-https://blogs.oracle.com/otnjp/post/recipes-for-using-the-optional-class-as-its-meant-to-be-used-ja
+### 参考
 
-- レシピ2：直接 `get()` を呼ばない
+[Optionalクラスを意図されたとおりに使うための12のレシピ](https://blogs.oracle.com/otnjp/post/recipes-for-using-the-optional-class-as-its-meant-to-be-used-ja)
+
+直接 `get()` を呼ばない
 - `Optional` は空になる可能性があるため、`get()` を直接呼び出すと `java.util.NoSuchElementException` がスローされる
+- 後述の `orElse()` や `orElseGet()` 等を使う
+
+`orElse()` 内で高コストの演算をしない
+- `orElse()` が返す値は、オプショナル値が存在するかどうかによらず、必ず評価されます
+- そこで、`orElseGet(Supplier<? extends T> supplier)` を使います。このメソッドは `orElse()` に似ていますが、1つ違いがあります。`Optional` が空だった場合、`orElse()` では直接デフォルト値が返されますが、`orElseGet()` には `Optional` が空だった場合にのみ呼び出される `Supplier` 関数を渡すことができます。
+
+値が存在しない場合に別の `Optional` を返すときは `or (Supplier<? extends Optional<? extends T>> supplier)` を使う
+- https://docs.oracle.com/javase/jp/17/docs/api/java.base/java/util/Optional.html#or(java.util.function.Supplier)
 
 
